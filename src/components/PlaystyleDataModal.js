498bca80d9b33f542560dca2838e8163ac0b9c87
RhichelleStrauss <251169@virtualwindow.co.za>
2026-03-28 23:38:01 +0200

    styling data modal
    styled an imported react modal that explains data on playstyle chart

--- src/components/PlaystyleDataModal.js
+++ src/components/PlaystyleDataModal.js
@@ -1,10 +1,11 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import React from 'react';
+import '../css/PlaystyleDataModal.css';
 
 function MyVerticallyCenteredModal(props) {
   return (
-    <Modal
+    <Modal className='modalStyle'
       {...props}
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
@@ -12,21 +13,54 @@ function MyVerticallyCenteredModal(props) {
     >
       <Modal.Header closeButton>
 
-        <Modal.Title id="contained-modal-title-vcenter">
-          Explain Data
+        <Modal.Title id="contained-modal-title-vcenter" 
+        style={{color: '#F20775', fontSize: '22px', fontFamily: "beaufort-pro" }}>
+          Explain Playstyle Profile data
         </Modal.Title>
 
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal
+        <h4 style={{color: '#F20775', fontSize: '22px', fontFamily: "beaufort-pro" }}>
+            Sweep Rate
         </h4>
 
         <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
+          The percent rate of how many times the team has won a clean sweep, a clean sweep is when 
+          the team wins a series without losing a single game.
         </p>
-        
+
+         <h4 style={{color: '#F20775', fontSize: '22px', fontFamily: "beaufort-pro" }}>Game 1 Winrate
+        </h4>
+
+        <p>
+          The winrate of the team when they win the entire series after winning the first game in the series.
+        </p>
+
+         <h4 style={{color: '#F20775', fontSize: '22px', fontFamily: "beaufort-pro" }}>Comeback Rate
+        </h4>
+
+        <p>
+          The winrate of the team when they win the entire series after losing the first game in the series.
+          (making a comeback after losing)
+        </p>
+
+
+         <h4 style={{color: '#F20775', fontSize: '22px', fontFamily: "beaufort-pro" }}>SilverScrapes Winrate
+        </h4>
+
+        <p>
+          SilverScrapes is a league esports term for when a series reaches the tie-breaker game. SilverScrapes
+          winrate is the winrate of the team, after reaching silverscrapes and winning the series 
+        </p>
+
+         <h4 style={{color: '#F20775', fontSize: '22px', fontFamily: "beaufort-pro" }}>Recent matches volume
+        </h4>
+
+        <p>
+          Recent matches played
+        </p>
+
+
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
@@ -57,6 +91,7 @@ function PlaystyleDataModal() {
       <MyVerticallyCenteredModal
         show={modalShow}
         onHide={() => setModalShow(false)}
+        dialogClassName="custom-modal-width"
       />
     </>
   );
